15798b6a6116c7e62e1a43d39657b39b49d271b3
Shuvo Goswami <avitalukder20158@gmail.com>
2021-06-02 07:55:23 +0600

    responsive done

--- src/Components/Home/MainHeader/MainHeader.js
+++ src/Components/Home/MainHeader/MainHeader.js
@@ -3,16 +3,14 @@ import HeaderMain from '../../Images/headerMain.jpg';
 
 const MainHeader = () => {
     return (
-        <div className="">
-            <div className="row d-flex justify-content-center align-items-center p-5" >
-                <div className="col-md-5 col-sm-6 col-12">
-                    <img src={HeaderMain} alt="" className="img-fluid" />
-                </div>
-                <div className="col-md-7 col-sm-6 col-12 text-white">
-                    <h1 className="text-uppercase">Life is an Event. Make it Memorable.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, similique?</p>
-                    <button className="btn btn-primary">GET US</button>
-                </div>
+        <div className="row d-flex align-items-center justify-content-center p-5" >
+            <div className="col-md-5 col-sm-6 col-12">
+                <img src={HeaderMain} alt="" className="img-fluid" />
+            </div>
+            <div className="col-md-7 col-sm-6 col-12 text-white">
+                <h1 className="text-uppercase">Life is an Event. Make it Memorable.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, similique?</p>
+                <button className="btn btn-primary">GET US</button>
             </div>
         </div>
     );
